d0dd93e820fb9af813595d5afea30ab801e0c7e1
ClarenceKuo <mkuo8@wisc.edu>
2019-10-19 15:20:03 -0500

    Add templates folder for page randering.

--- webapp/app.py
+++ webapp/app.py
@@ -1,14 +1,43 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello world'
+    return render_template("WebHome.html")
 
-@app.route("/salvador")
+@app.route('/Login')
+def index():
+    return render_template("index.html")
+
+@app.route('/Register')
+def index():
+    return render_template("index.html")
+
+@app.route('/Tracker')
+def index():
+    return render_template("index.html")
+
+@app.route('/Profile')
+def index():
+    return render_template("index.html")
+
+@app.route('/Tips')
+def index():
+    return render_template("index.html")
+
+@app.route('/About')
+def index():
+    return render_template("index.html")
+
+@app.route('/Ranking')
+def index():
+    return render_template("index.html")
+"""
+@app.route("/Clarence")
 def salvador():
-    return "Hello, Salvador"
+    return "Hello, Clarence"
+"""
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
