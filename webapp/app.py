8f5e487b78283dab5a3ee1a77517ad79973d60c4
ClarenceKuo <mkuo8@wisc.edu>
2019-10-19 16:02:11 -0500

    First merge: File structure interface

--- webapp/app.py
+++ webapp/app.py
@@ -2,38 +2,46 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    return render_template("WebHome.html")
+    return render_template("home.html")
 
+"""
 @app.route('/Login')
 def index():
     return render_template("index.html")
+
 
 @app.route('/Register')
 def index():
     return render_template("index.html")
 
+
 @app.route('/Tracker')
 def index():
     return render_template("index.html")
+
 
 @app.route('/Profile')
 def index():
     return render_template("index.html")
 
+
 @app.route('/Tips')
 def index():
     return render_template("index.html")
+
 
 @app.route('/About')
 def index():
     return render_template("index.html")
 
+
 @app.route('/Ranking')
 def index():
     return render_template("index.html")
-"""
+
 @app.route("/Clarence")
 def salvador():
     return "Hello, Clarence"
